2945f52694040031372949fa8082909760e73589
Mackenzie Pech <mackenzieerinpech@gmail.com>
2020-05-26 17:00:16 -0400

    gsoc

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+    import axios from 'axios'
+    import Pusher from 'pusher-js'
+    import Chart from '../components/Chart'
+    import VoteButtons from '../components/VoteButtons'
 
-export default class Index extends React.Component {
+    var pusher = new Pusher('app_key', {
+      cluster: 'cluster',
+      encrypted: true
+    })
+    const channel = pusher.subscribe('pet-wars')
 
-  render () {
-    return (
-      <div>
-        Hello world
-      </div>
-    )
-  }
-}
+    export default class Index extends React.Component {
+      constructor (props) {
+        super(props)
+        this.state = {
+          data: [0, 0, 0]
+        }
+      }
+
+      render () {
+        return (
+          <div>
+            <Chart data={this.state.data} />
+            <VoteButtons handleVote={this.handleVote.bind(this)} />
+          </div>
+        )
+      }
+    }
