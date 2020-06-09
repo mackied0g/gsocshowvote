4953ca33295dc77f98559ef2629f217442a902a0
Mackenzie Pech <mackenzieerinpech@gmail.com>
2020-06-08 21:26:22 -0400

    i tried so hard and got so far but in the end it doesnt even matter i had to fall to lose it all but in the end it doesnt even matter one thing i dont know why it doesnt even matter how hard you try keep that in mind i designed this rhyme to explain in due time

--- components/Chart.js
+++ components/Chart.js
@@ -46,3 +46,4 @@ export default class Chart extends React.Component {
         />
     )
   }
+}
--- components/VoteButtons.js
+++ components/VoteButtons.js
@@ -5,11 +5,14 @@ import React from 'react'
       render () {
         return (
           <div className={'vote-button-group'}>
-            <button className={'vote-button'} onClick={() => this.props.handleVote({vote: 'dogs'})}>Vote Dogs</button>
+            <button className={'vote-button'} onClick={() => 
+              this.props.handleVote({vote: 'dogs'})}>Vote Dogs</button>
 
-            <button className={'vote-button'} onClick={() => this.props.handleVote({vote: 'cats'})}>Vote Cats</button>
+            <button className={'vote-button'} onClick={() => 
+              this.props.handleVote({vote: 'cats'})}>Vote Cats</button>
 
-            <button className={'vote-button'} onClick={() => this.props.handleVote({vote: 'hamsters'})}>Vote Hamsters</button>
+            <button className={'vote-button'} onClick={() => 
+              this.props.handleVote({vote: 'hamsters'})}>Vote Hamsters</button>
           </div>
         )
       }
